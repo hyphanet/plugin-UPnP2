bc6a422599492f8bc05b48be8e27f2eb7136e087
Florent Daigniere <nextgens@freenetproject.org>
2020-08-11 21:14:53 +0200

    ensure that the version string is set from gradle

--- src/main/java/plugins/UPnP2/UPnP2.java
+++ src/main/java/plugins/UPnP2/UPnP2.java
@@ -282,7 +282,7 @@ public class UPnP2 implements FredPlugin, FredPluginThreadless, FredPluginIPDete
 
     @Override
     public String getVersion() {
-        return "0.0.2";
+        return UPnP2.class.getPackage().getImplementationVersion();
     }
 
     // ###################################
